89beea388c756a5e6fa100946467ee32cadeca9a
dev-marko <markospasenovski00@gmail.com>
2022-09-02 17:49:22 +0200

    hotfix 3

--- FloraEducationAPI/Startup.cs
+++ FloraEducationAPI/Startup.cs
@@ -91,10 +91,10 @@ namespace FloraEducationAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
             app.UseHttpsRedirection();
 
