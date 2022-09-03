205c8efdf156064a191f878f6dda564da2a24fe1
dev-marko <markospasenovski00@gmail.com>
2022-09-03 13:04:22 +0200

    refactor: startup

--- FloraEducationAPI/Startup.cs
+++ FloraEducationAPI/Startup.cs
@@ -3,23 +3,14 @@ using FloraEducationAPI.Repository.Implementations;
 using FloraEducationAPI.Repository.Interfaces;
 using FloraEducationAPI.Service.Implementations;
 using FloraEducationAPI.Service.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FloraEducationAPI
 {
@@ -38,7 +29,6 @@ namespace FloraEducationAPI
             services.AddControllers();
 
             // Database connection
-            //$"Host=ec2-34-246-86-78.eu-west-1.compute.amazonaws.com;Database=d3s6emd1cr2pkl;Username=blnbhlkjtaexph;Password=63bfac8895daee59c9a8482c2633f198e67b4d6b4a4a30b9383af970376989fb;SSL Mode=Require;Trust Server Certificate=true";
 
             var dbConnectionString = string.Empty;
 
@@ -49,17 +39,9 @@ namespace FloraEducationAPI
             else
             {
                 // Use connection string provided at runtime by Heroku.
-                //var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-                var connectionUrl = "postgres://blnbhlkjtaexph:63bfac8895daee59c9a8482c2633f198e67b4d6b4a4a30b9383af970376989fb@ec2-34-246-86-78.eu-west-1.compute.amazonaws.com:5432/d3s6emd1cr2pkl";
+                var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                 connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
-                //var userPassSide = connectionUrl.Split("@")[0];
-                //var hostSide = connectionUrl.Split("@")[1];
-
-                //var user = userPassSide.Split(":")[0];
-                //var password = userPassSide.Split(":")[1];
-                //var host = hostSide.Split("/")[0];
-                //var database = hostSide.Split("/")[1].Split("?")[0];
 
                 var pgUserPass = connectionUrl.Split("@")[0];
                 var pgHostPortDb = connectionUrl.Split("@")[1];
@@ -72,7 +54,6 @@ namespace FloraEducationAPI
                 var pgPort = pgHostPort.Split(":")[1];
 
                 dbConnectionString = $"Host={pgHost};Database={pgDb};Port={pgPort};Username={pgUser};Password={pgPass};SSL Mode=Require;Trust Server Certificate=true";
-                //dbConnectionString = $"Host=ec2-34-246-86-78.eu-west-1.compute.amazonaws.com;Database=d3s6emd1cr2pkl;Username=blnbhlkjtaexph;Password=63bfac8895daee59c9a8482c2633f198e67b4d6b4a4a30b9383af970376989fb;SSL Mode=Require;Trust Server Certificate=true";
             }
 
             services.AddDbContext<FloraEducationDbContext>(options => options.UseNpgsql(dbConnectionString));
@@ -98,6 +79,7 @@ namespace FloraEducationAPI
             // CORS
             services.AddCors();
 
+            // HTTPS Redirect headers
             services.Configure<ForwardedHeadersOptions>(options =>
             {
                 options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
