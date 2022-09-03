a8164f1a0fa1aa605fec8f497545f2114c79cf59
dev-marko <markospasenovski00@gmail.com>
2022-09-03 12:26:29 +0200

    refactor: dynamic db connection

--- FloraEducationAPI/Startup.cs
+++ FloraEducationAPI/Startup.cs
@@ -38,34 +38,31 @@ namespace FloraEducationAPI
             services.AddControllers();
 
             // Database connection
+            //$"Host=ec2-34-246-86-78.eu-west-1.compute.amazonaws.com;Database=d3s6emd1cr2pkl;Username=blnbhlkjtaexph;Password=63bfac8895daee59c9a8482c2633f198e67b4d6b4a4a30b9383af970376989fb;SSL Mode=Require;Trust Server Certificate=true";
 
-            var dbConnectionString = $"Host=ec2-34-246-86-78.eu-west-1.compute.amazonaws.com;Database=d3s6emd1cr2pkl;Username=blnbhlkjtaexph;Password=63bfac8895daee59c9a8482c2633f198e67b4d6b4a4a30b9383af970376989fb;SSL Mode=Require;Trust Server Certificate=true";
-            ////Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development"
-            //if (false)
-            //{
-            //    dbConnectionString = Configuration.GetConnectionString("FloraEducationApiDatabase");
-            //}
-            //else
-            //{
-            //    // Use connection string provided at runtime by Heroku.
-            //    //var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-            //    var connectionUrl = "postgres://blnbhlkjtaexph:63bfac8895daee59c9a8482c2633f198e67b4d6b4a4a30b9383af970376989fb@ec2-34-246-86-78.eu-west-1.compute.amazonaws.com:5432/d3s6emd1cr2pkl";
+            var dbConnectionString = string.Empty;
 
-            //    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
-            //    var userPassSide = connectionUrl.Split("@")[0];
-            //    var hostSide = connectionUrl.Split("@")[1];
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+            {
+                dbConnectionString = Configuration.GetConnectionString("FloraEducationApiDatabase");
+            }
+            else
+            {
+                // Use connection string provided at runtime by Heroku.
+                var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                //var connectionUrl = "postgres://blnbhlkjtaexph:63bfac8895daee59c9a8482c2633f198e67b4d6b4a4a30b9383af970376989fb@ec2-34-246-86-78.eu-west-1.compute.amazonaws.com:5432/d3s6emd1cr2pkl";
 
-            //    var user = userPassSide.Split(":")[0];
-            //    var password = userPassSide.Split(":")[1];
-            //    var host = hostSide.Split("/")[0];
-            //    var database = hostSide.Split("/")[1].Split("?")[0];
+                connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+                var userPassSide = connectionUrl.Split("@")[0];
+                var hostSide = connectionUrl.Split("@")[1];
 
-            //    dbConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
-            //    Console.WriteLine(dbConnectionString);
-            //}
+                var user = userPassSide.Split(":")[0];
+                var password = userPassSide.Split(":")[1];
+                var host = hostSide.Split("/")[0];
+                var database = hostSide.Split("/")[1].Split("?")[0];
 
-
-
+                dbConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+            }
 
             services.AddDbContext<FloraEducationDbContext>(options => options.UseNpgsql(dbConnectionString));
 
